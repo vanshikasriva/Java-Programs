4df14c4fc8eb0f34da746fb655816b9172178485
srivastava <s.srivastava1322@gmail.com>
2026-06-04 01:02:23 +0530

    adding day 022 class

--- src/Day001/L00_variables.java
+++ src/Day001/L00_variables.java
@@ -4,7 +4,7 @@ public class L00_variables {
     static void main(String[] args) {
         byte age =87;
         byte _ = 9;
-        byte age =90; //unique name
+        byte age1 =90; //unique name
 //        D  I     L
 
     }
